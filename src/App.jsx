78a3c8032918631d5a60dbc2e463dfb4493800a0
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-12-14 01:42:43 +0530

    Add details and animation to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import About from './components/About'
-import Contact from './components/Contact'
-import Experience from './components/Experience'
-import Milestone from './components/milestones';
-import Projects from './components/projects';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
 
 function App() {
-    return (
-        <div>
-          <p></p>
-          <About />
-          <p></p>
-          <Milestone />
-          <p></p>
-          <Experience/>
-          <p></p>
-          <Projects/>
-          <p></p>
-          <Contact />
-        </div>
-      );
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
