7395ca4e122a2fa6445513ccd5b3db9f0f565752
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-12-17 01:33:15 +0530

    Modify personal details and animation

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Aishwarya Hariharan Iyer", "a Programmer", "a Poet", "a Developer", "a Dreamer" ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,8 +56,12 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hi! I'm`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Aishwarya Hariharan Iyer", "a Programmer", "a Poet", "a Developer", "a Dreamer"]'><span className="wrap">{text}</span></span></h1>
+                  <p>
+                    I am a <b>Year 2 Computer Science student</b> from the National University of Singapore and am enrolled in the <b>University Scholars' Programme</b>.
+                    You may find me pouring coffee and pouring over a project at 4 P.M or a poem at 4 A.M., but regardless of where you find me, you shall always find me passionate
+                    about <b>creativty that contributes to a cause</b>.
+                  </p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
